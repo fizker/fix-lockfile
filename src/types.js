7201a6329383cc8c88600bd3d6f38e52702055af
Benjamin Horsleben <benjamin@fizkerinc.dk>
2018-12-22 02:23:24 +0100

    Forced all types to be immutable

--- src/types.js
+++ src/types.js
@@ -1,6 +1,6 @@
 // @flow strict
 
-export type Package = {|
+export type Package = $ReadOnly<{|
 	version: string,
 	integrity: string,
 	resolved: string,
@@ -8,17 +8,17 @@ export type Package = {|
 	optional?: boolean,
 	requires?: { [string]: string },
 	dependencies?: { [string]: Package },
-|}
+|}>
 
-export type Lockfile = {
+export type Lockfile = $ReadOnly<{
 	name: string,
 	version: string,
 	lockfileVersion: number,
 	requires: boolean,
 	dependencies: { [string]: Package },
-}
+}>
 
-export type Diff = {
+export type Diff = $ReadOnly<{
 	path: $ReadOnlyArray<string>,
 	integrityOnly: boolean,
-}
+}>
