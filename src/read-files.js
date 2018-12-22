ea3667436aa7687e75bcffec25bd817e5a0c66f7
Benjamin Horsleben <benjamin@fizkerinc.dk>
2018-12-22 02:51:13 +0100

    Refactored the compare-files func to only read the files

--- src/read-files.js
+++ src/read-files.js
@@ -2,16 +2,18 @@
 
 import { promises as fs } from 'fs'
 import { catFile } from './git'
-import diffPackages from './diff-packages'
 
-import type { Diff, Lockfile } from './types'
+import type { Lockfile } from './types'
 
-export default async function(filePath:string) : Promise<$ReadOnlyArray<Diff>> {
+export default async function(filePath:string) : Promise<{ current: Lockfile, original: Lockfile }> {
 	const originalRawFile = await catFile(filePath)
 	const currentRawFile = await fs.readFile(filePath, { encoding: 'utf8' })
 
 	const parsedOriginal:Lockfile = JSON.parse(originalRawFile)
 	const parsedCurrent:Lockfile = JSON.parse(currentRawFile.toString())
 
-	return diffPackages(parsedOriginal, parsedCurrent)
+	return {
+		current: parsedCurrent,
+		original: parsedOriginal,
+	}
 }
