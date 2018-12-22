afbdbf5181fccbcfaa20dad6b0c454c441f64c92
Benjamin Horsleben <benjamin@fizkerinc.dk>
2018-12-22 02:57:48 +0100

    Updated the index-file to execute the code

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // @flow
 require('@babel/register')
+
+const [ , , file ] = process.argv
+
+const run = require('./src/run').default
+run(file)
+.then(() => console.log('done'))
+.catch(console.error)
